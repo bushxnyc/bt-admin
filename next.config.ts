4d97dead25483e5259216256e8ab27dc55502106
Brian Dwayne <brian@blktouch.com>
2025-10-21 11:00:23 -0400

    Security update

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
           {
             key: "Content-Security-Policy",
             value:
-              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self'; connect-src 'self' *.blktouch.com;",
+              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' blob:; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self'; connect-src 'self' *.blktouch.com; worker-src 'self' blob:;",
           },
         ],
       },
