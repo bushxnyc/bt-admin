84ba4f8ef03b3e3629be97652dbafe831891fd66
Brian Dwayne <brian@blktouch.com>
2026-03-22 00:01:41 -0400

    Attempt to fix the loop

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,12 +1,7 @@
 import { UserButton } from "@clerk/nextjs";
-import { auth } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
 import { ReactNode, Suspense } from "react";
 
-export default async function DashboardLayout({ children }: { children: ReactNode }) {
-  const { userId } = await auth();
-  if (!userId) redirect("/sign-in");
-
+export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <header className="flex justify-end items-center p-4 gap-4 h-16">
